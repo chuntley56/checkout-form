ee0f256af39f4ca985ed489627910d4ca1eb6748
chuntley56 <coreyhuntley562@gmail.com>
2018-09-10 17:33:33 -0700

    add sameasshipping

--- src/components/checkout.js
+++ src/components/checkout.js
@@ -1,32 +1,38 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import serializeForm from 'form-serialize';
 
 class CheckoutForm extends Component {
   state = {
     billingName: '',
     billingAddress: '',
     shippingName: '',
-    shippingAddress: ''
+    shippingAddress: '',
+    shippingSameAsBilling: 'checked'
  };
 
+handleSubmit = event => {
+  event.preventDefault();
 
-handleChange = event => {
-    // this.setState({ event.target.value })
-    // console.log(this.state);
+  const values = serializeForm(event.target, { hash: true});
+
+  console.log(values);
 }
+
+
 
   render() {
     const {
       billingName,
       billingAddress,
       shippingName,
-      shippingAddress
+      shippingAddress,
+      shippingSameAsBilling
     } = this.state;
-console.log(this.state.billingName);
 
     return(
       <div>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <label>Billing Address</label>
           <input
           name="name"
@@ -37,13 +43,39 @@ console.log(this.state.billingName);
           />
           <input
           name="address"
-          type="text" placeholder="Address" defaultValue={billingAddress} />
+          type="text"
+          placeholder="Address"
+          defaultValue={billingAddress}
+          onChange={event => this.setState({billingAddress: event.target.value})}
+          />
           <button>Submit</button>
         </form>
-        <form onSubmit={this.handleChange}>
+
+        <form>
+        <input
+        name="shipcheck"
+        type="checkbox"
+        defaultChecked={shippingSameAsBilling}
+        onClick={event => this.setState({shippingSameAsBilling: event.target.checked})}
+        />
+        <label>Same as billing</label>
           <label>Shipping Address</label>
-          <input name="name" type="text" placeholder="Name" defaultValue={shippingName} />
-          <input name="address" type="text" placeholder="Address" defaultValue={shippingAddress} />
+          <input
+          name="shipName"
+          id="shipName"
+          type="text"
+          placeholder="Name"
+          value={shippingSameAsBilling ? billingName : shippingName}
+          onChange={event => this.setState({shippingName: event.target.value})}
+          />
+          <input
+          name="shipAddress"
+          id="shipAddress"
+          type="text"
+          placeholder="Address"
+          value={shippingSameAsBilling ? billingAddress : shippingAddress}
+          onChange={event => this.setState({shippingAddress: event.target.value})}
+          />
           <button>Submit</button>
         </form>
       </div>
